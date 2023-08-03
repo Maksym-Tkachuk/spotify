e69be6268988b4a388b0d058fec4272c55c36087
BohdanSh <shemigonbogdanwork@gmail.com>
2023-08-03 22:13:08 +0300

    Added app icon and styled launch screen

--- App.tsx
+++ App.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import { useEffect } from 'react'
 import { SafeAreaView, Text } from 'react-native'
 import { ThemeProvider } from 'styled-components'
 import theme from './src/theme'
+import SplashScreen from 'react-native-splash-screen'
 
 const App = () => {
+  useEffect(SplashScreen.hide, [])
+
   return (
     <SafeAreaView style={{ padding: 20 }}>
       <ThemeProvider theme={theme}>
